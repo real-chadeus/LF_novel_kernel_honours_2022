3cb337feccb426cb89142de5391a361c6b075c9f
saze <saze@protonmail.com>
2022-07-24 20:51:31 +1000

    some refactor, minor updates and start building model

--- kernel/lfi_se_net.py
+++ kernel/lfi_se_net.py
@@ -17,6 +17,14 @@ def LF_conv_block(inputs, n_filters=4, filter_size=(3,3), n_sais=49, stride=2):
     X = layers.Conv3D(n_filters, 1, padding='same', activation='relu')(X)
     return X
 
+def build_model(input_shape, summary=True):
+    # build the model using Tensorflow.
+    X = layers.Conv3D(filters=3, kernel_size=(3,3,3), strides=(1,2,2), padding='same')(inputs) 
+    X = tf.nn.relu(x)
+    
+    X = LF_conv_block(X, n_filters=3, filter_size=(3,3)
+    X = layers.MaxPooling3D(pool_size=(1,2,2))
+
 class LFSEBlock(tf.keras.Model):
     # light field squeeze-and-excitation block
     # comes after all the convolution blocks
@@ -24,12 +32,13 @@ class LFSEBlock(tf.keras.Model):
         super(LFSEBlock).__init__(name='light_field_se_block')
     
     def call(self, input_tensor, training=True):
-        x =   
-        return tf.nn.relu(x)
-         
+        return tf.nn.linear(X)
 
 def build_model(shape):
     inputs = layers.Input(shape=shape, name='lfi_input')  
+
+
+
 
 
 
