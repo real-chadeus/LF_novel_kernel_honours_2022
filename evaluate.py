91c70c98ac5391719300f268fba7d068e48a1ea4
saze <saze@protonmail.com>
2022-08-12 12:52:38 +1000

    add plotting

--- evaluate.py
+++ evaluate.py
@@ -9,6 +9,7 @@ import preprocessing.hci_dataset_tools.file_io as hci_io
 import kernel.lfi_se_net as se_net
 import tensorflow.keras.losses as losses
 import argparse
+import plots
 
 load_path = 'models/'
 input_shape = (3,436,3,436,3)
@@ -16,7 +17,7 @@ hci = load.load_hci(img_shape=input_shape)
 model = keras.models.load_model(load_path + 'model0')
 metrics = model.evaluate(x=hci[0], y=hci[1], batch_size=1, workers=4)
 hist = model.history
-print(hist.keys())
+plots.plot_mse('model1')
 print(metrics)
 
 
---
+++ plots.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+save_dir = 'models/'
+
+def plot_mse(model_name='model1'):
+    load_dir = save_dir + model_name + '/'
+    f = pd.read_csv(load_dir + 'history.csv')
+    mse = f['val_mean_squared_error']
+    plt.plot(mse)
+    plt.ylabel('validation mean squared error')
+    plt.xlabel('epoch')
+    plt.show()
+
+    mse = f['mean_squared_error']
+    plt.plot(mse)
+    plt.ylabel('training mean squared error')
+    plt.xlabel('epoch')
+    plt.show()
+    
+     
+def plot_mae(model_name='model1'):
+    load_dir = save_dir + model_name + '/'
+    f = pd.read_csv(load_dir + 'history.csv')
+    mse = f['val_mean_absolute_error']
+    plt.plot(mse)
+    plt.ylabel('validation mean absolute error')
+    plt.xlabel('epoch')
+    plt.show()
+
+    mse = f['mean_absolute_error']
+    plt.plot(mse)
+    plt.ylabel('training mean absolute error')
+    plt.xlabel('epoch')
+    plt.show()
+
+
+
+
+
